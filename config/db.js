66c2d5fa52149a28d4538e697bf846b6506f2155
sachinkumar012 <sachinyadav887780@gmail.com>
2025-04-09 16:57:13 +0530

    adding backend on git

---
+++ config/db.js
@@ -0,0 +1,8 @@
+import mongoose from "mongoose";
+
+export const  connectDB = async () =>{
+    await mongoose.connect('mongodb+srv://sachinyadav887780:Yadav909707@cluster0.avf4ase.mongodb.net/quickledish').then(()=>console.log("DB Connected"))
+}
+
+// add your mongoDB connection string above.
+// Do not use '@' symbol in your databse user's password else it will show an error.
